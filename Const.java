5e1bd0b5daf162e4f2ff6db17488bd531bf94690
lausen <lausen@gmail.com>
2012-05-18 16:39:28 +0000

    const

---
+++ Const.java
@@ -0,0 +1,16 @@
+package com.pigmal.android.ex.twitter4j;
+
+public class Const {
+	static String CONSUMER_KEY = "";
+	static String CONSUMER_SECRET = "";
+
+	static String PREFERENCE_NAME = "twitter_oauth";
+	static final String PREF_KEY_SECRET = "";
+	static final String PREF_KEY_TOKEN = "";
+	
+	static final String CALLBACK_URL = "oauth://t4jsample";
+	
+	static final String IEXTRA_AUTH_URL = "https://api.twitter.com/oauth/authorize";
+	static final String IEXTRA_OAUTH_VERIFIER = "oauth_verifier";
+	static final String IEXTRA_OAUTH_TOKEN = "oauth_token";
+}
